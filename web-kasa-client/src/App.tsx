4adb626c488a7c2c0eba8bb0d6d8801c831cf6fc
Furkan Baştan <121354889+furkanbastan@users.noreply.github.com>
2024-07-12 19:27:57 +0000

    router object and LoginPage created

--- web-kasa-client/src/App.tsx
+++ web-kasa-client/src/App.tsx
@@ -1,14 +1,15 @@
-import { AppLoading } from "./components/app-loading";
+import { RouterProvider } from "react-router-dom";
 import { LanguageContextProvider } from "./contexts/LanguageContext";
 import { ThemeContextProvider } from "./contexts/ThemeContext";
 import { StyleProvider } from "./utils/StyleProvider";
+import { router } from "./router";
 
 function App() {
   return (
     <StyleProvider>
       <LanguageContextProvider>
         <ThemeContextProvider>
-          <AppLoading />
+          <RouterProvider router={router} />
         </ThemeContextProvider>
       </LanguageContextProvider>
     </StyleProvider>
