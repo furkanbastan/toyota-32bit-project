9bf184420c7b1b1042d8be2a1bdbd344221619d0
Furkan Baştan <121354889+furkanbastan@users.noreply.github.com>
2024-07-21 11:31:36 +0300

    Sales Page width changed

--- web-kasa-client/src/containers/sales-page-container/styled.tsx
+++ web-kasa-client/src/containers/sales-page-container/styled.tsx
@@ -1,9 +1,10 @@
 import { styled } from "@mui/joy";
 
 export const SalesPageContainer = styled("div")(({}) => ({
-  width: "100%",
-  height: "100vh",
-  display: "flex",
-  gap: "var(--sales-page-gap)",
-  padding: "var(--sales-page-gap)",
+	width: "min(1500px, 100%)",
+	marginInline: "auto",
+	height: "100vh",
+	display: "flex",
+	gap: "var(--sales-page-gap)",
+	padding: "var(--sales-page-gap)",
 }));
