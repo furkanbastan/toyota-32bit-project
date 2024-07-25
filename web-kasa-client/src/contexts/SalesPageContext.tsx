8532d0d314e0705e35b94f3646bd82670bf07d59
Furkan Baştan <121354889+furkanbastan@users.noreply.github.com>
2024-07-25 22:34:33 +0300

    Sales Page Context modified

--- web-kasa-client/src/contexts/SalesPageContext.tsx
+++ web-kasa-client/src/contexts/SalesPageContext.tsx
@@ -2,8 +2,18 @@ import React from "react";
 
 // interfaces
 interface ISalesPageContext {
-	state?: {};
-	actions?: {};
+	state?: {
+		inputCode: string;
+		inputKeyboard: string;
+		adet: number;
+	};
+	actions?: {
+		setInputCode: (code: string) => any;
+		setInputKeyboard: (keyboard: string) => any;
+		setAdet: (adet: number) => any;
+		clearInputCode: () => any;
+		clearInputKeyboard: () => any;
+	};
 }
 
 // context
@@ -11,8 +21,32 @@ const SalesPageContext = React.createContext<ISalesPageContext>({});
 
 // provider
 function SalesPageContextProvider({ children }: any) {
+	const [inputCode, setInputCode] = React.useState<string>("");
+	const [inputKeyboard, setInputKeyboard] = React.useState<string>("");
+	const [adet, setAdet] = React.useState<number>(1);
+
+	const context: ISalesPageContext = {
+		state: {
+			inputCode,
+			inputKeyboard,
+			adet,
+		},
+		actions: {
+			setInputCode: (code) => setInputCode(code),
+
+			setInputKeyboard: (keyboard) => setInputKeyboard(keyboard),
+
+			setAdet: (adet) => setAdet(adet),
+
+			clearInputCode: () => setInputCode(""),
+
+			clearInputKeyboard: () => setInputKeyboard(""),
+		},
+	};
 	return (
-		<SalesPageContext.Provider value={{}}>{children}</SalesPageContext.Provider>
+		<SalesPageContext.Provider value={context}>
+			{children}
+		</SalesPageContext.Provider>
 	);
 }
 
--- web-kasa-client/src/pages/SalesPage.tsx
+++ web-kasa-client/src/pages/SalesPage.tsx
@@ -1,7 +1,15 @@
 import { SalesPageContainer } from "../containers/sales-page-container";
+import { BasketContextProvider } from "../contexts/BasketContext";
+import { SalesPageContextProvider } from "../contexts/SalesPageContext";
 
 function SalesPage() {
-  return <SalesPageContainer />;
+	return (
+		<SalesPageContextProvider>
+			<BasketContextProvider>
+				<SalesPageContainer />
+			</BasketContextProvider>
+		</SalesPageContextProvider>
+	);
 }
 
 export { SalesPage };
